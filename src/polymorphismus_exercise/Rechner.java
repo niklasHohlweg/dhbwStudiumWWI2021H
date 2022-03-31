1eba7e71f37cb18eef2411f230c5bcb1270d09fb
Niklas Hohlweg <niklas.hohlweg@be-terna.com>
2022-03-31 11:37:58 +0200

    Rechner klasse

--- src/polymorphismus_exercise/Rechner.java
+++ src/polymorphismus_exercise/Rechner.java
@@ -2,24 +2,44 @@ package polymorphismus_exercise;
 
 public class Rechner {
 
-    private Form formen;
+    private Form[] formen = new Form [10];
     private int counter = 0;
+
+
+    public void setArray(Form pFormen){
+
+        formen[counter] = pFormen;
+        counter += 1;
+
+    }
 
     public void printFlaechen(){
 
+        for(Form elem : formen){
 
+            System.out.println("Die Flaeche beträgt: " + elem.flaeche());
+
+        }
 
     }
 
     public void printUmfang(){
 
-        System.out.println("Der Umfang beträgt " + formen.umfang());
+        for(Form elem : formen){
+
+            System.out.println("Der  Umfang beträgt: " + elem.umfang());
+
+        }
 
     }
 
     public void printFarben(){
 
-        System.out.println("Die Farbe ist: " + formen.getFarbe());
+        for(Form elem : formen){
+
+            System.out.println("Die Farbe ist: " + elem.getFarbe());
+
+        }
 
     }
 
