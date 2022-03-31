52afb0b3e0d094389f12a590a2e451e6d09284af
Niklas Hohlweg <niklas.hohlweg@be-terna.com>
2022-03-31 12:03:23 +0200

    polymorphismus_excercise FINISH

---
+++ src/polymorphismus_exercise/Interaktion.java
@@ -0,0 +1,65 @@
+package polymorphismus_exercise;
+
+public class Interaktion {
+
+    public static void main(String[] args) {
+
+        Rechner myRechner = new Rechner();
+
+        for (int i = 0; i < 10; i++) {
+
+            Form formen;
+
+            int color = (int) Math.round(Math.random() * 3 + 1);
+            int type = (int) Math.round(Math.random() * 1 + 1);
+            int numb = (int) Math.round(Math.random() * 9 + 1);
+
+            if (type == 1) {
+
+                formen = new Kreis();
+                formen = (Kreis) formen;
+
+                ((Kreis) formen).setRadius(numb);
+
+
+            } else {
+
+                formen = new Quadrat();
+                formen = (Quadrat) formen;
+
+                ((Quadrat) formen).setKantenLaenge(numb);
+
+
+            }
+
+            switch (color) {
+
+                case 1:
+                    formen.setFarbe("rot");
+                    break;
+
+                case 2:
+                    formen.setFarbe("grün");
+                    break;
+
+                case 3:
+                    formen.setFarbe("blau");
+                    break;
+
+                case 4:
+                    formen.setFarbe("lila");
+                    break;
+
+            }
+            myRechner.setArray(formen);
+
+
+
+        }
+        myRechner.printFarben();
+        myRechner.printFlaechen();
+        myRechner.printUmfang();
+
+    }
+
+}
