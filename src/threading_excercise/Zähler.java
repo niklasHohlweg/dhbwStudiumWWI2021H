aeedef29ff844daab00a1883ad1194df07367093
Niklas Hohlweg <niklas.hohlweg@be-terna.com>
2022-04-21 10:45:12 +0200

    finish exercise threading Niklas

---
+++ src/threading_excercise/Zähler.java
@@ -0,0 +1,25 @@
+package threading_excercise;
+
+public class Zähler implements Runnable{
+    /**
+     * When an object implementing interface {@code Runnable} is used
+     * to create a thread, starting the thread causes the object's
+     * {@code run} method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method {@code run} is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+
+        for(int i = 0; i <= 1000000; i++){
+
+
+            System.out.println(i);
+        }
+
+    }
+}
