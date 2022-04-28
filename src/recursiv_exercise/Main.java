4f047102c08780cfd4062bfb3b055bc1a734582f
MaxGanzert <wi21257@lehhre.dhbw-stuttgart.de>
2022-04-28 13:37:36 +0200

    finish commit Thread Exercise

--- src/recursiv_exercise/Main.java
+++ src/recursiv_exercise/Main.java
@@ -7,10 +7,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("n: ");
         int n = sc.nextInt();
-        System.out.println("n!: "+fakultaet(n));
+        System.out.println("n!: "+fakultaetR(n));
+        System.out.println("n!: "+fakultaetI(n));
     }
-    private static long fakultaet(int n){
+    private static long fakultaetR(int n){
         if(n ==0 || n==1){return 1;}
-        else{return n*fakultaet(n-1);}
+        else{return n*fakultaetR(n-1);}
+    }
+    private static long fakultaetI(int n){
+        long result =1;
+        if(n ==0 || n==1){return 1;}
+        else{
+            for (int i = 2; i <= n; i++) {
+                result *= i;
+            }
+            return result;
+        }
     }
 }
