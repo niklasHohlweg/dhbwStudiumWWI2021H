ca948a78f11517d139bf2a3a1795b8b2a76875b8
MaxGanzert <wi21257@lehhre.dhbw-stuttgart.de>
2022-05-05 13:36:32 +0200

    finish commit TreeNode_methods_exercise Max

---
+++ src/treeNode_methods_exercise/TreeNode.java
@@ -0,0 +1,14 @@
+package treeNode_methods_exercise;
+
+public class TreeNode<T> {
+    private TreeNode<T> left = null;
+    private TreeNode<T> right = null;
+    private T element;
+    public TreeNode(T element){ this.element = element;}
+    public TreeNode<T> getLeft(){return left;}
+    public void setLeft(TreeNode<T> left){this.left =left;}
+    public TreeNode<T> getRight(){return right;}
+    public void setRight(TreeNode<T> right){this.right = right;}
+    public T getElement(){return element;}
+    public void setElement(T element){this.element= element;}
+}
