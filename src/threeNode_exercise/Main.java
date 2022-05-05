02eb2aacbbb6c78b8db2ef94c72893f35b800bc7
Niklas Hohlweg <niklas.hohlweg@be-terna.com>
2022-05-05 10:49:38 +0200

    finish commit treeNode Niklas

--- src/threeNode_exercise/Main.java
+++ src/threeNode_exercise/Main.java
@@ -4,6 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
+        TreeNode root = new TreeNode('L');
+        root.setLeft(new TreeNode('A'));
+        root.getLeft().setLeft(new TreeNode('H'));
+        root.getLeft().setRight(new TreeNode<>('L'));
+
+        root.setRight(new TreeNode('O'));
+        root.getRight().setRight(new TreeNode('!'));
+
+        System.out.println(root.getElement());
+        System.out.println(root.getLeft().getElement());
+        System.out.println(root.getLeft().getLeft().getElement());
+        System.out.println(root.getLeft().getRight().getElement());
+        System.out.println(root.getRight().getElement());
+        System.out.println(root.getRight().getRight().getElement());
+
     }
 
 }
